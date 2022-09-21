67f1a9784c8e38b4d9ac0e80cffa86292e6a038b
Kai <kai520958@gmail.com>
2022-09-21 15:05:11 -0500

    ComponentController

--- Assets/Scripts/ComponentController.cs
+++ Assets/Scripts/ComponentController.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ComponentController : MonoBehaviour
 {
-    [SerializeField]
-    private Dictionary<string, bool> _tempName;
+    private Dictionary<System.Type, bool> _behaviorList;
+
+    public void EnableAndDisableComponents()
+    {
+        foreach (var pair in _behaviorList)
+        {
+            var behavior = GetComponent(pair.Key) as Behaviour;
+            if (behavior != null)
+                behavior.enabled = pair.Value;
+            else
+                Debug.LogError("Unable to find behavior " + pair.Key);
+        }
+    }
+
+    public void SetBehaviorList(Dictionary<System.Type, bool> behaviorList)
+    {
+        _behaviorList = behaviorList;
+    }
 }
